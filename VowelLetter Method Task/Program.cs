5a0bf3abd49d7e37c8ba26d07401eee6e7e250e8
nijatmammadovv <tu7q78xlv@code.edu.az>
2022-03-08 12:53:25 +0400

    Add project files.

---
+++ VowelLetter Method Task/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace VowelLetter_Method_Task
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Write a sentence ");
+            string text = Console.ReadLine();
+            Console.WriteLine("{1} sentence contains {0} VowelLetter..", VowelLetter(text),text);
+            Console.ReadLine();
+        }
+
+        static int VowelLetter(string text)
+        {
+            int count = 0;
+            char[] VowelLetter = { 'a', 'ə', 'e', 'o', 'ö', 'u', 'ü', 'ı', 'i','A', 'E','Ə','O', 'Ö', 'U', 'Ü', 'I', 'İ',};
+            foreach (char c in text)
+            {
+                foreach (char letter in VowelLetter)
+                {
+                    if (c == letter) 
+                        count++;
+                }
+            }
+            return count;
+        }
+    }
+}
